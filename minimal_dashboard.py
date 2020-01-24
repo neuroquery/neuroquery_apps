aee24911a8ae735f7c6f5cc5e6ff05fac07112fb
Romain PRIMET <romain.primet@inria.fr>
2020-01-24 09:38:33 +0100

    drafting UI (early stages, not functional yet)

--- minimal_dashboard.py
+++ minimal_dashboard.py
@@ -1,28 +1,28 @@
-# Minimal example: encoding with NeuroQuery
-# =========================================
+# Encoding with NeuroQuery
+# ========================
 # 
 # The model used here is the same as the one deployed on the neuroquery website
 # ( https://neuroquery.saclay.inria.fr ).
-# You can run this example on binder: https://mybinder.org/v2/gh/neuroquery/neuroquery.git/master?filepath=examples
 
 # ## Encode a query into a statistical map of the brain
+
 from neuroquery import fetch_neuroquery_model, NeuroQueryModel
-from nilearn.plotting import view_img
+from nilearn.plotting import plot_img, view_img
+import ipywidgets as widgets
+from IPython.display import display
 
 encoder = NeuroQueryModel.from_data_dir(fetch_neuroquery_model())
 
-#-
-query = """Huntington's disease"""
+query = widgets.Text(value="brainstem")
+button = widgets.Button(description="Run query")
+display(query)
+display(button)
+output = widgets.Output()
+
 result = encoder(query)
-view_img(result["z_map"], threshold=3.1)
+plot_img(result["z_map"], threshold=3.1)
 
 # (drag the mouse on this interactive plot to see other slices)
-#
-# Note: if you are not using a jupyter notebook, use `.open_in_browser()` to
-# open the plot above:
-
-view_img(result["z_map"], threshold=3.1).open_in_browser()
-
 
 # ## Display some relevant terms:
 
