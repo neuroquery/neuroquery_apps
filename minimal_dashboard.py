e94f90da62931c71350bfa2ed0ac242a6381129d
Romain PRIMET <romain.primet@inria.fr>
2020-01-27 16:30:56 +0100

    clickable links for publications

--- minimal_dashboard.py
+++ minimal_dashboard.py
@@ -35,20 +35,22 @@ display(widgets.HBox([query, button]))
 output = widgets.Output()
 display(output)
 
+def title_as_link(df):
+    return df.apply(lambda x: f"<a href=\"{x['pubmed_url']}\" target=\"_blank\">{x['title']}</a>", axis=1)
+
 def run_and_display_query(_):
     result = encoder(query.value)
+    similar_docs = result["similar_documents"].head().copy()
+    similar_docs.loc[:, 'title'] = title_as_link(similar_docs)
     with output:
         output.clear_output()
         display_html(view_img(result["z_map"], threshold=3.1).get_iframe(), raw=True)
         sw = result["similar_words"].head(12)
         display(Markdown("## Similar Words"))
         display(sw.style.bar(subset=['weight_in_brain_map', 'weight_in_query'], color='lightgreen'))
-        sdocs = result["similar_documents"][['title', 'similarity']].head()
         display(Markdown("## Similar Documents"))
-        display(sdocs.style.hide_index().bar(color='lightgreen'))
+        display(similar_docs[['title', 'similarity']].style.hide_index().bar(color='lightgreen'))
 
 button.on_click(run_and_display_query)
 
 run_and_display_query(None)
-
-
