607edb3b11cb9129aec66524c2927f0341304517
Jerome Dockes <jerome@dockes.org>
2020-02-15 07:33:08 +0100

    tweak some details in minimal dashboard

--- minimal_dashboard.py
+++ minimal_dashboard.py
@@ -23,6 +23,7 @@
 # ## Encode a query into a statistical map of the brain
 
 from neuroquery import fetch_neuroquery_model, NeuroQueryModel
+from neuroquery.tokenization import get_html_highlighted_text
 from nilearn.plotting import plot_img, view_img
 import ipywidgets as widgets
 from IPython.display import display, display_html, Markdown
@@ -30,7 +31,15 @@ from IPython.display import display, display_html, Markdown
 # %%capture
 encoder = NeuroQueryModel.from_data_dir(fetch_neuroquery_model())
 
-query = widgets.Text(value="brainstem")
+example_query = """Prosopagnosia,
+also called face blindness,[2] is a cognitive disorder of face
+perception in which the ability to recognize familiar faces, including one's own
+face (self-recognition), is impaired, while other aspects of visual processing
+(e.g., object discrimination) and intellectual functioning (e.g.,
+decision-making) remain intact. (from wikipedia)
+""".replace("\n", " ")
+
+query = widgets.Textarea(value=example_query)
 button = widgets.Button(description="Run query")
 display(widgets.HBox([query, button]))
 output = widgets.Output()
@@ -41,14 +50,16 @@ def title_as_link(df):
 
 def run_and_display_query(_):
     result = encoder(query.value)
-    similar_docs = result["similar_documents"].head().copy()
+    similar_docs = result["similar_documents"].head(20).copy()
     similar_docs.loc[:, 'title'] = title_as_link(similar_docs)
     with output:
         output.clear_output()
-        display_html(view_img(result["z_map"], threshold=3.1).get_iframe(), raw=True)
-        sw = result["similar_words"].head(12)
+        display_html(
+            get_html_highlighted_text(result["highlighted_text"]), raw=True)
+        display_html(view_img(result["brain_map"], threshold=3.1).get_iframe(), raw=True)
+        sw = result["similar_words"].head(12).drop("weight_in_query", axis=1)
         display(Markdown("## Similar Words"))
-        display(sw.style.bar(subset=['weight_in_brain_map', 'weight_in_query'], color='lightgreen'))
+        display(sw.style.bar(subset=['similarity', 'weight_in_brain_map'], color='lightgreen', width=95))
         display(Markdown("## Similar Documents"))
         display(similar_docs[['title', 'similarity']].style.hide_index().bar(color='lightgreen'))
 
